a938d81af0f25eab45418758b002dd9ee7e29fa8
Marco Herrera <marcohg0312@gmail.com>
2024-05-28 11:36:35 -0600

    Se me habia olvidado subir esto jaja

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from classifier import load_training_objs
-from training import train_dataset
+from training import train_dataset, test_process_with_no_pca
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -23,7 +23,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
     # Entrenamos el modelo y obtenemos el dataset reducido usado para el entrenamiento
-    X_pca = train_dataset(X_train, y_train)
+    X_pca = train_dataset(X_train, y_train, 2)
 
     plot_scatter(X_pca, y, 'PC1', 'PC2', 'income', 'Reduced Dataset Using PCA')
 
@@ -32,6 +32,22 @@ def main():
     # Cargamos los objetos en el módulo de clasificación
     load_training_objs()
 
-    print(get_model_accuracy(X_test, y_test))
+    print("Tasa de aciertos con 2 componentes: ", get_model_accuracy(X_test, y_test))
+    print("\n")
+
+    # Ahora hacemos la prueba con 3 componentes
+    X_pca = train_dataset(X_train, y_train, 3)
+    load_training_objs()
+    print("Tasa de aciertos con 3 componentes: ", get_model_accuracy(X_test, y_test))
+    print("\n")
+
+    # Ahora hacemos la prueba con 13 componentes
+    X_pca = train_dataset(X_train, y_train, 13)
+    load_training_objs()
+    print("Tasa de aciertos con 13 componentes: ", get_model_accuracy(X_test, y_test))
+    print("\n")
+
+    # Ahora se prueba el proceso sin PCA
+    test_process_with_no_pca(X_train, y_train, X_test, y_test)
 
 main()
--- training.py
+++ training.py
@@ -8,16 +8,20 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
 import numpy as np
 import joblib
 import os
+import time
 from constants import TRAINING_OBJS_DIR
 
-# Recibe el dataset y el nombre de la variable que se desea predecir
+
+# Recibe el dataset, el nombre de la variable que se desea predecir y la cantidad de componentes
 # Entrena un modelo utilizando PCA
-# Para este caso se usan 2 PCA
 # Retorna el dataset reducido utilizado para el entrenamiento
-def train_dataset(X_train, Y_train):
+def train_dataset(X_train, Y_train, n_components):
+
+    start_time = time.time()
     
     # Codificamos las variables cualitativas
     ordinal_encoder = OrdinalEncoder(handle_unknown = 'use_encoded_value', unknown_value = np.nan)
@@ -28,11 +32,11 @@ def train_dataset(X_train, Y_train):
     X_imputed = imputer.fit_transform(X_encoded)
     
     # Estandarizamos los datos
-    scaler = StandardScaler(with_mean=False)
+    scaler = StandardScaler(with_mean = False)
     X_scaled = scaler.fit_transform(X_imputed)
     
-    # Ejecutamos un PCA para reducir la dimensionalidad a 2
-    pca = PCA(n_components = 2)
+    # Ejecutamos un PCA para reducir la dimensionalidad
+    pca = PCA(n_components = n_components)
     X_pca = pca.fit_transform(X_scaled)
     
     # Entrenamos un modelo reconocedor
@@ -46,4 +50,35 @@ def train_dataset(X_train, Y_train):
     joblib.dump(model, os.path.join(TRAINING_OBJS_DIR, 'logistic_regression_model.pkl'))
     joblib.dump(imputer, os.path.join(TRAINING_OBJS_DIR, 'imputer.pkl'))
 
+    print(f"Tiempo de ejecución con {n_components} componentes: ", time.time() - start_time)
+
     return X_pca
+
+# Prueba el proceso de entrenamiento y clasificación sin PCA
+def test_process_with_no_pca(X_train, Y_train, X_test, Y_test):
+
+    # Codificamos las variables cualitativas
+    ordinal_encoder = OrdinalEncoder(handle_unknown = 'use_encoded_value', unknown_value = np.nan)
+    X_encoded = ordinal_encoder.fit_transform(X_train)
+
+    # Reemplazamos datos faltantes por la media
+    imputer = SimpleImputer(strategy = 'mean')
+    X_imputed = imputer.fit_transform(X_encoded)
+    
+    # Estandarizamos los datos
+    scaler = StandardScaler(with_mean = False)
+    X_scaled = scaler.fit_transform(X_imputed)
+    
+    # Entrenamos un modelo reconocedor
+    model = RandomForestClassifier()
+    model.fit(X_scaled, Y_train)
+
+    # Preprocesamos los datos de entrenamiento
+    data_encoded = ordinal_encoder.transform(X_test)
+    data_imputed  = imputer.transform(data_encoded)
+    data_scaled = scaler.transform(data_imputed)
+
+    Y_pred = model.predict(data_scaled)
+    accuracy = accuracy_score(Y_test, Y_pred)
+
+    print("Tasa de aciertos sin PCA: ", accuracy)
